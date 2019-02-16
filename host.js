0880f7e23b9a71cf281bea50b0540e5fc14b80cb
siemiatj <kuba@saskla.cz>
2019-02-16 20:30:04 +0100

    - start package for plugin example

---
+++ host.js
@@ -0,0 +1,17 @@
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var config = require('./webpack.config');
+
+var listenHost = process.env.DOCKER ? '0.0.0.0' : 'localhost';
+
+new WebpackDevServer(webpack(config), {
+    publicPath: config.output.publicPath,
+    hot: true,
+    historyApiFallback: true
+}).listen(3002, listenHost, function (err) {
+    if (err) {
+        return console.error(err);
+    }
+
+    return console.warn('Listening at http://'+listenHost+':3002/');
+});
